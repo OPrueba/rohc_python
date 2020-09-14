0ecd4bc2fcee8ed29728dd3b45f19d4dfd6870c4
OPrueba <36871509+OPrueba@users.noreply.github.com>
2020-09-14 08:29:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@
 from distutils.core import setup, Extension
 import sys
 
-# check the system version because there was problems with version before
-# 2.6
-if sys.hexversion < 0x02060000:
-    print "Version 2.6 or greater is recommended - edit setup.py to remove warning"
-    sys.exit()
+requires = [
+    python_version >= "2.6.0"'
+]
 
 setup(name='ROHC',
       version='0.1',
       description='Python Robust Header Compression (ROHC) Module',
       author='Joseph Ishac',
       author_email='jishac@nasa.gov',
+      install_requires=requires,
       ext_modules=[Extension('rohc', ['src/rohc.c'], libraries=['rohc'])],
       )
 
